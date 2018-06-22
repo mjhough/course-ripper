17e2af13c7f6ef09a7fa4085ae3207d6ca2e3b36
Matt Hough <matt@hough.tv>
2018-06-22 12:59:23 +1000

    Download lecture files successfully

--- lib/course_ripper.rb
+++ lib/course_ripper.rb
@@ -5,17 +5,28 @@ require_relative '../config/environment'
 
 include Capybara::DSL
 
-Capybara.default_driver = :selenium
+Capybara.register_driver :custom_driver do |app|
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  profile['browser.download.dir'] = "~/Lectures/MATH1050" # Hard coding
+  profile['browser.download.folderList'] = 2
+  profile['browser.helperApps.alwaysAsk.force'] = false
+  profile['browser.download.manager.showWhenStarting'] = false
+  profile['browser.helperApps.neverAsk.saveToDisk'] = "video/mp4"
+  profile['csvjs.disabled'] = true
+  Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+end
+
+Capybara.default_driver = :custom_driver
 
 link = "http://learn.uq.edu.au"
 
 visit link
-fill_in('username', with: 's4478978')
-fill_in('password', with: 'Kooka2024')
+fill_in('username', with: 'USERNAME') # Hard coding
+fill_in('password', with: 'NOPE')  # Hard coding
 click_on('LOGIN')
 sleep 5
 click_link('Courses')
-fill_in('searchText', with: 'MATH1050')
+fill_in('searchText', with: 'MATH1050') # Hard coding
 click_on('Go')
 current_course = page.all('#listContainer_datatable tr').last
 current_course_name = current_course.text
@@ -26,18 +37,18 @@ File.open("ANNOUNCEMENTS: #{current_course_name}", 'w') do |file|
 end
 
 click_on('Learning Resources')
-sleep 5
+sleep 1.5
 click_on('Lecture_Recordings')
+sleep 3
 
 within_frame 'content' do
+  find('.select-typeahead').click
+  find('.active-result', text: 'Oldest').click
   page.all('.menu').each do |lecture|
     lecture.click
     click_link('Download original')
-    page.all('.screenOptions a').first(2).each do |video_opt|
-      select find(:select).text.split("\n")[1]
-      click_on('DOWNLOAD')
-    end
+    sleep 1.5
+    select find(:select).text.split("\n")[1]
+    click_on('Download')
   end
 end
-
-
